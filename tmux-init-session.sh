135905b2bfb932de253bc835d8dedea563e4cedd
Brett Jones <r.brett.jones@gmail.com>
2025-11-17 15:41:34 -0500

    fix alias completion git

--- tmux-init-session.sh
+++ tmux-init-session.sh
@@ -4,23 +4,33 @@
 SESSION=$(tmux display-message -p '#S')
 
 # Only run if this is the first window (to avoid running multiple times)
-WINDOW_COUNT=$(tmux list-windows -t "$SESSION" | wc -l)
+WINDOW_COUNT=$(tmux list-windows -t "$SESSION" | wc -l | tr -d ' ')
 if [ "$WINDOW_COUNT" -gt 1 ]; then
     exit 0
 fi
 
-# Set the default directory for all windows (change this to your preferred path)
-DEFAULT_DIR="$HOME"
+# Add a small delay to ensure tmux is fully initialized
+sleep 0.5
 
-# Create 4 windows, all starting in the same directory
-tmux rename-window -t "$SESSION:1" "1"
-tmux send-keys -t "$SESSION:1" "cd '$DEFAULT_DIR'" Enter
-tmux send-keys -t "$SESSION:1" "clear" Enter
+# Define directories for each window
+DIRECTORIES=(
+    "$HOME/src/Lyft-iOS"
+    "$HOME/src/Lyft-iOS2"
+    "$HOME/src/Lyft-iOS3"
+    "$HOME/src/Lyft-iOS4"
+)
 
-for i in {2..4}; do
-    tmux new-window -t "$SESSION" -n "$i" -c "$DEFAULT_DIR"
-    tmux send-keys -t "$SESSION:$i" "clear" Enter
+# Get the first window index (works regardless of base-index setting)
+FIRST_WINDOW=$(tmux list-windows -t "$SESSION" -F "#{window_index}" | head -n1)
+
+# Set up the first window
+tmux rename-window -t "$SESSION:$FIRST_WINDOW" "lyft1"
+tmux send-keys -t "$SESSION:$FIRST_WINDOW" "cd '${DIRECTORIES[0]}'" Enter
+
+# Create 3 more windows
+for i in {1..3}; do
+    tmux new-window -t "$SESSION" -n "lyft$((i+1))" -c "${DIRECTORIES[$i]}"
 done
 
 # Select the first window
-tmux select-window -t "$SESSION:1" 
+tmux select-window -t "$SESSION:$FIRST_WINDOW" 
